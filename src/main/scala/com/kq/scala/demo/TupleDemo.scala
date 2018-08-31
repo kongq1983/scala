46b33ea2a2671d822b5a545b856ad65e3dfb5f2b
king <king@qq.com>
2018-08-31 09:01:06 +0800

    add TupleDemo

---
+++ src/main/scala/com/kq/scala/demo/TupleDemo.scala
@@ -0,0 +1,12 @@
+package com.kq.scala.demo
+
+object TupleDemo {
+
+  def main(args: Array[String]): Unit = {
+      var pair = (88,"king")
+      printf("[1]=%s\n",pair._1)
+      printf("[2]=%s\n",pair._2)
+
+  }
+
+}
