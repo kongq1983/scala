c3fd69366f58e72b1a37685e7e5ca5ae922e3394
king <king@qq.com>
2019-01-24 10:49:05 +0800

    add string demo

---
+++ src/main/scala/com/kq/scala/demo/StringDemo.scala
@@ -0,0 +1,18 @@
+package demo
+
+/**
+  * StringDemo
+  *
+  * @author kq
+  * @date 2019-01-24
+  */
+object StringDemo {
+
+  def main(args: Array[String]): Unit = {
+      var str = "welcome to scala!"
+      println(str)
+      var arg = Array("Scala","Kafka","Spark");
+      arg.foreach(a=> println(a))
+  }
+
+}
